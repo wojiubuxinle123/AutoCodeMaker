289c9e76ed75c7aff64429a4a70e1e4ca71fd113
WangXing <attackthefate@163.com>
2020-11-06 22:01:20 +0800

    'init'

---
+++ java/src/main/java/com/xxxspring/XxxspringApplication.java
@@ -0,0 +1,13 @@
+package com.xxxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxxspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxxspringApplication.class, args);
+    }
+
+}
---
+++ java/src/test/java/com/xxxspring/XxxspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxxspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
